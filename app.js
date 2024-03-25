ece970a69a03f76cf6876098ed9c0750de3dc301
Arpan Maji <arpan@codelogicx.com>
2024-03-25 20:48:21 +0530

    cors origin placed before routes

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const allowCrossDomain = function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 // closure function for success
 app.use(successHandler);
 
@@ -17,21 +33,6 @@ app.use(errorHandler);
 
 app.use('/api', require('./routes'))
 
-const allowCrossDomain = function (req, res, next) {
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
-    // intercept OPTIONS method
-    if ("OPTIONS" == req.method) {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-};
-
-app.use(allowCrossDomain);
 
 app.listen(port,()=>{
     console.log('listening on port '+ port);
